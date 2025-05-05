72b4cf52124e984a8c65bcce486e6a78781e2211
YamnaSarwar123 <165307599+YamnaSarwar123@users.noreply.github.com>
2025-05-05 16:30:29 +0500

    Initial commit - reset history

---
+++ 00-intro-python/01_add_to_number.py
@@ -0,0 +1,15 @@
+def main():
+
+    num1 = input ("enter the first number:")
+    num1 = int(num1)
+
+    num2 = input ("enter the second number:")
+    num2 = int(num2)
+
+    total_sum = num1 + num2
+
+    print(f"the sum of the two numbers is:{total_sum}")
+
+    
+if __name__ == "__main__":
+    main()
